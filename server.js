0b5f4445e755107fb13398710c4207502e389551
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-11-27 00:10:33 +0200

    add ws

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const expressWs = require('express-ws');
 
 const app = express();
-expressWs(app);
+
+app.use(
+  /\/.*\/ws\//,
+  createProxyMiddleware({
+    target: 'ws://167.99.246.103:8080',
+    changeOrigin: true,
+    ws: true,
+    pathRewrite: (path) => path.replace(/\/ws\//, '/'),
+  })
+);
 
 app.use(
   '/*',
@@ -12,29 +20,6 @@ app.use(
     changeOrigin: true,
   })
 );
-
-app.ws('/*', (ws, req) => {
-  console.log('WebSocket connection established');
-
-  const target = 'ws://167.99.246.103:8080';
-  const wsProxy = new WebSocket(target);
-
-  wsProxy.on('message', (data) => {
-    ws.send(data);
-  });
-
-  ws.on('message', (data) => {
-    wsProxy.send(data);
-  });
-
-  ws.on('close', () => {
-    wsProxy.close();
-  });
-
-  wsProxy.on('error', (error) => {
-    console.error('WebSocket proxy error:', error);
-  });
-});
 
 app.listen(3001, () => {
   console.log('Сервер запущен на порту 3001');
