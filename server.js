84f519f48b7adb9d99179e9167c81a0655bb8cc7
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-11-26 13:58:49 +0200

    add ws

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const expressWs = require('express-ws');
 
 const app = express();
+expressWs(app);
 
 app.use(
   '/*',
@@ -10,6 +12,23 @@ app.use(
     changeOrigin: true,
   })
 );
+
+app.ws('/*', (ws, req) => {
+  const target = 'ws://167.99.246.103:8080';  
+  const wsProxy = new WebSocket(target);
+
+  wsProxy.on('message', (data) => {
+    ws.send(data);
+  });
+
+  ws.on('message', (data) => {
+    wsProxy.send(data);
+  });
+
+  ws.on('close', () => {
+    wsProxy.close();
+  });
+});
 
 app.listen(3001, () => {
   console.log('Сервер запущен на порту 3001');
